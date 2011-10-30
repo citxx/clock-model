3d3e37ec33c311640b33524cd476ca1c094a9c45
Artem Tabolin <artemtab@yandex.ru>
2011-10-30 21:38:40 +0400

    'Rotation' class is implemented

---
+++ src/Rotation.cpp
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <GL/gl.h>
+
+#include "Rotation.hpp"
+
+Rotation::Rotation(const Quaternion &q):
+        r(q) {
+}
+
+Rotation::Rotation(float angle, const Vector &axis):
+        r(cosf(angle / 2), sinf(angle / 2) * normalized(axis)) {
+}
+
+Rotation::Rotation(float angle, float x, float y, float z):
+        r(cosf(angle / 2), sinf(angle / 2) * normalized(Vector(x, y, z))) {
+}
+
+void Rotation::glPerform() {
+    glRotatef(
+        acosf(this->r.w) * 360.0 / M_PI,
+        this->r.v.x,
+        this->r.v.y,
+        this->r.v.z
+    );
+}
+
+Rotation operator *(const Rotation &a, const Rotation &b) {
+    return Rotation(a.r * b.r);
+}
---
+++ src/Rotation.hpp
@@ -0,0 +1,22 @@
+#ifndef ROTATION_HPP
+#define ROTATION_HPP
+
+#include "Quaternion.hpp"
+#include "Vector.hpp"
+
+class Rotation {
+    Quaternion r;
+
+  public:
+    Rotation(const Quaternion &q);
+    Rotation(float angle, const Vector &axis);
+    Rotation(float angle, float x, float y, float z);
+
+    void glPerform();
+
+    friend Rotation operator *(const Rotation &a, const Rotation &b);
+};
+
+Rotation operator *(const Rotation &a, const Rotation &b);
+
+#endif  // ROTATION_HPP
